dfb53a315b489078ba7017e8336f14389e257084
Megan Tegeder <megan.tegeder@gmail.com>
2024-11-13 13:27:48 -0600

    printing most recent when not empty

--- server/modules/calculations.js
+++ server/modules/calculations.js
@@ -3,9 +3,8 @@ let calculations = {
     recent: function(){
         console.log('in recent');
         if( this.all.length > 0 ){
-            return 'empty';
             // console.log('recent', this.all[ this.all.length - 1]);
-            // return this.all[ this.all.length - 1];
+            return this.all[ this.all.length - 1];
         }
         else {
             return 'empty';
